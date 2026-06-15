062c569a866e81421abbf216f215d0caf46d1132
Sam Ritchie <11716061@amlab-cn001.localdomain>
2026-06-15 09:28:37 +0200

    def_3_18: ISigmaSeparation (formalized, 67min, turns=10)

--- leanification/Chapter3_GraphTheory.lean
+++ leanification/Chapter3_GraphTheory.lean
@@ -37,3 +37,4 @@ import Chapter3_GraphTheory.Section3_3.CollidersAndNon
 import Chapter3_GraphTheory.Section3_3.BlockableAndUnblockable
 import Chapter3_GraphTheory.Section3_3.AcyclicNonCollidersBlockable
 import Chapter3_GraphTheory.Section3_3.SigmaBlockedWalks
+import Chapter3_GraphTheory.Section3_3.ISigmaSeparation
