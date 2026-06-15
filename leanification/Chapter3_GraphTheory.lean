5c1e2d73bf30ef87b9206b7b934a6ca1461f0fa3
Sam Ritchie <11716061@amlab-cn001.localdomain>
2026-06-15 08:18:12 +0200

    def_3_17: SigmaBlockedWalks (formalized, 46min, turns=9)

--- leanification/Chapter3_GraphTheory.lean
+++ leanification/Chapter3_GraphTheory.lean
@@ -36,3 +36,4 @@ import Chapter3_GraphTheory.Section3_2.MarginalizingOutThe
 import Chapter3_GraphTheory.Section3_3.CollidersAndNon
 import Chapter3_GraphTheory.Section3_3.BlockableAndUnblockable
 import Chapter3_GraphTheory.Section3_3.AcyclicNonCollidersBlockable
+import Chapter3_GraphTheory.Section3_3.SigmaBlockedWalks
