0cf9c8e938ace56d4ddff9442ff00b9bc9991d5f
Sam Ritchie <11716061@amlab-cn001.localdomain>
2026-06-21 19:03:27 +0200

    claim_3_27: LabelRoman (proven, 1039min, turns=6)

--- leanification/Chapter3_GraphTheory.lean
+++ leanification/Chapter3_GraphTheory.lean
@@ -39,3 +39,4 @@ import Chapter3_GraphTheory.Section3_3.AcyclicNonCollidersBlockable
 import Chapter3_GraphTheory.Section3_3.SigmaBlockedWalks
 import Chapter3_GraphTheory.Section3_3.ISigmaSeparation
 import Chapter3_GraphTheory.Section3_3.SigmaSeparationSymmetric
+import Chapter3_GraphTheory.Section3_3.LabelRoman
